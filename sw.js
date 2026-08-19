b6f5d1c79ed82dda860f1b6fac069d96ed385629
TabibNet <aj4555822@gmail.com>
2026-08-20 01:20:40 +0300

    Import OneSignal SDK in sw.js
    Added OneSignal SDK import to service worker.

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 const CACHE_NAME = 'raheba-med-v24'; // تم تغيير الرقم لإجبار الهاتف على التحديث
 const CORE_ASSETS = [
   './',
