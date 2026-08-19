a4bad0258fa07cd82068b4acddea86ff414d6812
TabibNet <aj4555822@gmail.com>
2026-08-20 02:40:01 +0300

    Integrate OneSignal SDK into service worker
    Added OneSignal SDK and service worker event listeners.

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
 const CACHE_NAME = 'raheba-med-v27'; // تم تغيير الرقم لإجبار الهاتف على التحديث
 const CORE_ASSETS = [
   './',
