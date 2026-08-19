fb78fda86691e61135efea69c43014ce1ef24152
TabibNet <aj4555822@gmail.com>
2026-08-20 02:12:15 +0300

    Increment CACHE_NAME to v26
    Updated CACHE_NAME to force phone update.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
-const CACHE_NAME = 'raheba-med-v25'; // تم تغيير الرقم لإجبار الهاتف على التحديث
+const CACHE_NAME = 'raheba-med-v26'; // تم تغيير الرقم لإجبار الهاتف على التحديث
 const CORE_ASSETS = [
   './',
   './index.html',
