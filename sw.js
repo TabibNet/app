3175b7cc8450edd6cdee88dbfa7bdf06dcd80673
TabibNet <aj4555822@gmail.com>
2026-08-20 14:02:25 +0300

    Increment CACHE_NAME version to v32
    Updated CACHE_NAME to force service worker refresh.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 
-const CACHE_NAME = 'raheba-med-v31'; // تم رفع الرقم لإجبار الهاتف على تحديث الـ SW فوراً
+const CACHE_NAME = 'raheba-med-v32'; // تم رفع الرقم لإجبار الهاتف على تحديث الـ SW فوراً
 const CORE_ASSETS = [
   './',
   './index.html',
